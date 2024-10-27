126c5ee1e389b7524d630c1342ab23fa5a8aae9c
unit-404 <conway24b@gmail.com>
2024-10-27 20:25:35 +0700

    web-core library

--- src/$core$/Engine.ts
+++ src/$core$/Engine.ts
@@ -171,7 +171,54 @@ const classes: StyleTuple[] = [
 ];
 
 //
-whenAnyScreenChanges((e?: any) => {
-    updateOrientation(e);
-    setStyleRules(classes);
-});
+export const viewportHandler = (event?: any) => {
+    const layoutViewport = document.body;
+    const viewport = event?.target || visualViewport;
+
+    //
+    document.documentElement.style.setProperty(
+        "--visual-width",
+        (viewport?.width || 0) + "px",
+        ""
+    );
+
+    //
+    const vvh = viewport?.height;
+    const dff = vvh - (layoutViewport.getBoundingClientRect().height || window.innerHeight);
+    const cvh = Math.min(Math.max(vvh - dff, viewport?.offsetTop || 0) - (viewport?.offsetTop || 0), (screen.availHeight || screen.height));
+    document.documentElement.style.setProperty(
+        "--visual-height",
+        cvh + "px",
+        ""
+    );
+};
+
+//
+const initialize = ()=>{
+    //
+    window?.visualViewport?.addEventListener?.("scroll", viewportHandler);
+    window?.visualViewport?.addEventListener?.("resize", viewportHandler);
+
+    //
+    document.documentElement.addEventListener("fullscreenchange", viewportHandler);
+
+    //
+    if ("virtualKeyboard" in navigator) {
+        // @ts-ignore
+        navigator?.virtualKeyboard?.addEventListener?.(
+            "geometrychange",
+            viewportHandler,
+            {passive: true}
+        );
+    }
+
+    //
+    requestAnimationFrame(viewportHandler);
+    whenAnyScreenChanges((e?: any) => {
+        updateOrientation(e);
+        setStyleRules(classes);
+    });
+}
+
+//
+export default initialize;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-// TODO! Managed System!
+//
+export * from "./$core$/Engine";
+export * from "./$core$/StyleRules";
+export * from "./$core$/Properties";
+export * from "./$core$/Utils";
+
+//
+import run from "./$core$/Engine";
+export default run;
+
+//
+import "./$scss$/_Layers.scss";
+import "./$scss$/_Props.scss";
+import "./$scss$/_Root.scss";
+import "./$scss$/_Normalize.scss";
+import "./$scss$/_Zoom.scss";
