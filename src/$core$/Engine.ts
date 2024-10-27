9bdfb30eefe7fc385ae88745069d3385fd8688ae
unit-404 <conway24b@gmail.com>
2024-10-27 22:21:39 +0700

    pre-init css

--- src/$core$/Engine.ts
+++ src/$core$/Engine.ts
@@ -200,7 +200,7 @@ import styles from "../$scss$/_Module.scss?inline";
 const loadInlineStyle = (inline: string)=>{
     const style = document.createElement("style");
     //style.innerHTML = inline;
-    style.innerHTML = `@import(${URL.createObjectURL(new Blob([inline], {type: "text/css"}))})`;
+    style.innerHTML = `@import url("${URL.createObjectURL(new Blob([inline], {type: "text/css"}))}");`;
     document.head.appendChild(style);
 }
 
