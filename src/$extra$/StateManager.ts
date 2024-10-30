6279543b4fade99a67712be57063344fe7fc43c5
unit-404 <conway24b@gmail.com>
2024-10-30 21:25:12 +0700

    extras

---
+++ src/$extra$/StateManager.ts
@@ -0,0 +1,57 @@
+//import {subscribe} from "./ReactiveLib.ts";
+
+//
+export class StateManager {
+    elements = new WeakMap<HTMLElement, any>();
+    named = new Map<string, any>();
+
+    //
+    constructor() {
+    }
+
+    //
+    set(name: string, state: any, whatToDo: Function | null = null) {
+        this.named.set(name, state);
+        whatToDo?.(name, state);
+        return this;
+    }
+
+    //
+    bind(element: HTMLElement, state: any, whatToDo: Function | null = null) {
+        this.elements.set(element, state);
+        whatToDo?.(element, state);
+        return this;
+    }
+
+    //
+    get(element: HTMLElement | string) {
+        return element instanceof HTMLElement ? this.elements.get(element) : this.named.get(element);
+    }
+
+    //
+    /*stateBehave(element: HTMLElement | string, onChange: Function | null = null) {
+        // @ts-ignore
+        const state = (element instanceof HTMLElement ? this.elements : this.named).get(element);
+
+        //
+        if (element instanceof HTMLElement) {
+            const ref = new WeakRef(element);
+            subscribe(state, (value, prop)=>{
+                onChange?.(ref.deref(), prop, value);
+            });
+        } else {
+            subscribe(state, (value, prop)=>{
+                onChange?.(element, prop, value);
+            });
+        }
+
+        //
+        return this;
+    }*/
+}
+
+//
+const states = new StateManager();
+
+//
+export default states;
