0a0c9183f76bcb6c0c0acc3b8324ae03e5ba4644
u2re-dev <conway24b@yandex.ru>
2025-01-02 10:40:20 +0700

    reduce code

--- src/index.ts
+++ src/index.ts
@@ -4,50 +4,8 @@ export * from "./$ts$/TaskManager";
 // @ts-ignore
 import styles from "./$scss$/_Module.scss?inline&compress";
 
-//
-const OWNER = "core";
-
-//
-const setStyleURL = (base: [any, any], url: string)=>{
-    //
-    if (base[1] == "innerHTML") {
-        base[0][base[1]] = `@import url("${url}");`;
-    } else {
-        base[0][base[1]] = url;
-    }
-}
-
-//
-const hash = async (string: string) => {
-    const hashBuffer = await crypto.subtle.digest('SHA-256', new TextEncoder().encode(string));
-    return "sha256-" + btoa(String.fromCharCode.apply(null, new Uint8Array(hashBuffer) as unknown as number[]));
-}
-
-//
-const loadStyleSheet = async (inline: string, base?: [any, any], integrity?: string|Promise<string>)=>{
-    const url = URL.canParse(inline) ? inline : URL.createObjectURL(new Blob([inline], {type: "text/css"}));
-    if (base?.[0] && (!URL.canParse(inline) || integrity) && base?.[0] instanceof HTMLLinkElement) {
-        const I: any = (integrity ?? hash(inline));
-        if (typeof I?.then == "function") {
-            I?.then?.((H)=>base?.[0]?.setAttribute?.("integrity", H));
-        } else {
-            base?.[0]?.setAttribute?.("integrity", I as string);
-        }
-    }
-    if (base) setStyleURL(base, url);
-}
-
-//
-const loadBlobStyle = (inline: string)=>{
-    const style = document.createElement("link");
-    style.rel = "stylesheet";
-    style.type = "text/css";
-    style.crossOrigin = "same-origin";
-    style.dataset.owner = OWNER;
-    loadStyleSheet(inline, [style, "href"]);
-    document.head.appendChild(style);
-    return style;
-}
+// @ts-ignore
+import { loadBlobStyle } from "/externals/lib/dom.js";
 
 //
 const initialize = ()=>{
